f6376cea8c9053a678a02ccb45ab5dd5caff7c7f
Nqadhafi <qadhafi817@gmail.com>
2024-07-02 20:51:17 +0700

    Layouting

--- index.php
+++ index.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
     <title>Aplikasi Penggajian Karyawan</title>
 </head>
 <body>
-    Test
+    <div class="container-fluid">
+        <!-- Navbar start -->
+    <nav class="navbar bg-body-tertiary ">
+  <div class="container-fluid bg-primary ">
+    <a class="navbar-brand text-white p-3" href="#">
+     <h3>Sistem Penggajian Karyawan PT .Eka Maju</h3>
+    </a>
+  </div>
+</nav>
+    </div>
 </body>
+<script src="./css/bootstrap.min.js"></script>
 </html>
